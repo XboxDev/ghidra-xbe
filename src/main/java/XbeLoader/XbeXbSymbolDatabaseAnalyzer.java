2eaf48baaa9cf9780a3f1f11bd94746e92b3c29d
Stefan Schmidt <thrimbor.github@gmail.com>
2021-10-06 17:42:44 +0200

    Rename package to more descriptive name

--- src/main/java/XbeLoader/XbeXbSymbolDatabaseAnalyzer.java
+++ src/main/java/XbeLoader/XbeXbSymbolDatabaseAnalyzer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package skeleton;
+package xbeloader;
 
 import java.util.List;
 import java.util.*;
