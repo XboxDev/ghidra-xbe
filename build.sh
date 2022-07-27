9503962de00355ff3366769a2f666966297ef093
Ryan Wendland <wendland@live.com.au>
2022-07-27 17:48:12 +0930

    Update to Ghidra 10.1.5

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 export CORRETTO_ARCHIVE=amazon-corretto-15-x64-linux-jdk.tar.gz
 export CORRETTO_URL=https://corretto.aws/downloads/latest/${CORRETTO_ARCHIVE}
-export GHIDRA_VER_CORE=10.1.4
+export GHIDRA_VER_CORE=10.1.5
 export GHIDRA_VER=${GHIDRA_VER_CORE}_PUBLIC
-export GHIDRA_DATE=20220519
+export GHIDRA_DATE=20220726
 export GHIDRA_ARCHIVE=ghidra_${GHIDRA_VER}_${GHIDRA_DATE}.zip
 export GHIDRA_URL=https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${GHIDRA_VER_CORE}_build/${GHIDRA_ARCHIVE}
 export GRADLE_VER=6.8.2
